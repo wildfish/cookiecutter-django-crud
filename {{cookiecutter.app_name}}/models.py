380199b15ddfeb19f67a3dbb7428b52301b2136a
Rolo <rolo@wildfish.com>
2013-11-22 11:41:10 +0000

    Use __str__ for Python3 with Python2 compatibility decorator.

--- {{cookiecutter.app_name}}/models.py
+++ {{cookiecutter.app_name}}/models.py
@@ -1,13 +1,15 @@
+from __future__ import unicode_literals
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
 class {{ cookiecutter.model_name }}(models.Model):
     name = models.CharField(max_length=255)
 
-    def __unicode__(self):
+    @python_2_unicode_compatible
+    def __str__(self):
         return '{{ cookiecutter.model_name }} ({})'.format(self.id or 'Unsaved')
 
     def get_absolute_url(self):
         return reverse('{{ cookiecutter.model_name|lower }}_detail', args=[str(self.id)])
-
