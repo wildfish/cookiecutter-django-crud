bbf59bff8c9f989f6b5c6e00f7c8f15a4429a7e4
Rolo <rolo@wildfish.com>
2014-08-11 15:06:04 +0100

    Simplified views.py to remove CRUD base class and use reverse_lazy.

--- {{cookiecutter.app_name}}/views.py
+++ {{cookiecutter.app_name}}/views.py
@@ -1,34 +1,31 @@
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse_lazy
 from vanilla import ListView, CreateView, DetailView, UpdateView, DeleteView
 from .forms import {{ cookiecutter.model_name }}Form
 from .models import {{ cookiecutter.model_name }}
 
 
-class {{ cookiecutter.model_name }}CRUDView(object):
+class {{ cookiecutter.model_name }}List(ListView):
     model = {{ cookiecutter.model_name }}
-    form_class = {{ cookiecutter.model_name }}Form
     paginate_by = 20
 
-    def get_success_url(self):
-        return reverse('{{ cookiecutter.model_name|lower }}_list')
+
+class {{ cookiecutter.model_name }}Create(CreateView):
+    model = {{ cookiecutter.model_name }}
+    form_class = {{ cookiecutter.model_name }}Form
+    success_url = reverse_lazy('{{ cookiecutter.model_name|lower }}_list')
 
 
-class {{ cookiecutter.model_name }}List({{ cookiecutter.model_name }}CRUDView, ListView):
-    pass
+class {{ cookiecutter.model_name }}Detail(DetailView):
+    model = {{ cookiecutter.model_name }}
 
 
-class {{ cookiecutter.model_name }}Create({{ cookiecutter.model_name }}CRUDView, CreateView):
-    pass
+class {{ cookiecutter.model_name }}Update(UpdateView):
+    model = {{ cookiecutter.model_name }}
+    form_class = {{ cookiecutter.model_name }}Form
+    success_url = reverse_lazy('{{ cookiecutter.model_name|lower }}_list')
 
 
-class {{ cookiecutter.model_name }}Detail({{ cookiecutter.model_name }}CRUDView, DetailView):
-    pass
-
-
-class {{ cookiecutter.model_name }}Update({{ cookiecutter.model_name }}CRUDView, UpdateView):
-    pass
-
-
-class {{ cookiecutter.model_name }}Delete({{ cookiecutter.model_name }}CRUDView, DeleteView):
-    pass
+class {{ cookiecutter.model_name }}Delete(DeleteView):
+    model = {{ cookiecutter.model_name }}
+    success_url = reverse_lazy('{{ cookiecutter.model_name|lower }}_list')
 
